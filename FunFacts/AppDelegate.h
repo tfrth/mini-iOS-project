15f0216889cd37df3733c0ef08a0bdf9cb7c0add
Tyler Frith <tfrith@sjc-tfrith-mba.local>
2015-07-29 16:15:50 -0700

    Initial Commit

---
+++ FunFacts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunFacts
+//
+//  Created by Tyler Frith on 7/29/15.
+//  Copyright (c) 2015 tfrth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FunFacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunFacts
+//
+//  Created by Tyler Frith on 7/29/15.
+//  Copyright (c) 2015 tfrth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
